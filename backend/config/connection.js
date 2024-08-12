dec20841d01b1c6b8c943d0e2f2261c7e6695005
JEMEDINAL <medinalondon2004@gmail.com>
2024-08-12 15:48:27 -0500

    config new commit

--- backend/config/connection.js
+++ backend/config/connection.js
@@ -1,8 +1,16 @@
 const mongoose = require("mongoose")
 require("dotenv").config()
 
-//const URI = `mongodb+srv://${process.env.USERBD}:${process.env.PASSWORDBD}@adso2669736.unkkdqc.mongodb.net/${process.env.DB}`
+
 const URI= `mongodb+srv://${process.env.USERDB}:${process.env.PASSWORDDB}@adso2669736.unkkdqc.mongodb.net/${process.env.DB}`
 mongoose.connect(URI)
+
+mongoose.connect(URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    ssl: true, 
+})
+.then(() => console.log("Conexión exitosa a MongoDB"))
+.catch(err => console.error("Error al conectar con MongoDB", err));
 
 module.exports = mongoose 
