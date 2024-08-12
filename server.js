ed4b08017043ec4b56ef6a6eefeb45bbe124855d
sebastianEspino <sebasespinosa2016@gmail.com>
2024-08-12 09:48:23 -0500

    email

--- server.js
+++ server.js
@@ -1,94 +1,157 @@
 const express = require("express")
 const bodyParser = require('body-parser');
 const usuario = require("./backend/models/usuario.model")
+const productos = require("./backend/models/productos.model")
+const clientes = require("./backend/models/clientes.model")
 
 const logger = require("morgan")
 require("dotenv").config()
 var app = express()
-app.set('view engine','ejs')
+
 /*
 app.use(bodyParser.json())
 */
 app.set('view engine', 'ejs')
-app.set('views','./views')
-app.use(express.urlencoded({extended:false}))
+app.set('views', './views')
+app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
-app.get("/",async (req,res)=>{
-    const consulta = await usuario.find({})
+
+app.get("/",(req,res)=>{
+    res.send("hola")
+})
+
+// productos
+app.get("/productos", async (req, res) => {
+    const consulta = await productos.find({})
     res.status(200).json(consulta)
-})
+});
 
-app.get('/mostrar',(req,res) =>{
-    res.render('pages/index')
-})
 
-app.get("/:ref",async (req,res)=>{
+
+app.get("/productos/:ref", async (req, res) => {
     req.params.ref
-    let consulta = await usuario.findOne({referencia:req.params.ref})
+    let consulta = await productos.findOne({ referencia: req.params.ref })
     res.status(200).json(consulta)
 })
-/*
-app.post("/",async (req,res)=>{
-    const {correo , pass} = req.body
-    
-    const insercion = await usuario.create({correo:correo,pass:pass})
-    if(insercion){
-        res.status(200).json({"mensaje":"registro realizado"})
-    }else{
-        res.status(404).json({"mensaje":"no se realizo"})
 
+app.post("/productos", async (req, res) => {
+    const nuevoProducto = {
+        referencia: req.body.referencia,
+        nombre: req.body.nombre,
+        descripcion: req.body.descripcion,
+        precio: req.body.precio,
+        stock: req.body.stock,
+        imagen: req.body.imagen,
+        habilitado: req.body.habilitado
     }
-}) 
-    */
 
-app.post("/",async (req,res)=>{
-    const nuevoUsuario = {
-        referencia: req.body.referenciaProducto,
-        nombre: req.body.nombreProducto,
-        descripcion: req.body.descripcionProducto,
-        precio:  req.body.precioProducto,
-        stock: req.body.stockProducto,
-        Imagen: req.body.imagenProducto,
-        habilitado:req.body.habilitadoProducto
-    } 
-    
-    const insercion = await usuario.create(nuevoUsuario)
-    if(insercion){
-        res.status(200).json({"mensaje":"registro realizado"})
-    }else{
-        res.status(404).json({"mensaje":"no se realizo"})
-
-    }
-}) 
-
-app.put("/:id", async (req,res)=>{
-    const usuarioEditar = {
-        referencia: req.body.referenciaProducto,
-        nombre: req.body.nombreProducto,
-        descripcion: req.body.descripcionProducto,
-        precio:  req.body.precioProducto,
-        stock: req.body.stockProducto,
-        Imagen: req.body.imagenProducto,
-        habilitado:req.body.habilitadoProducto
-    }
-    let actualizar = await usuario.findOneAndUpdate({_id:req.params.id},usuarioEditar)
-    if(actualizar){
-        res.status(200).json({"mensaje":"actualizado exitoso"})
-    }else{
-        res.status(404).json({"mensaje":"no se realizo"})
+    const insercion = await productos.create(nuevoProducto)
+    if (insercion) {
+        res.status(200).json({ "mensaje": "registro realizado" })
+    } else {
+        res.status(404).json({ "mensaje": "no se realizo" })
 
     }
 })
 
-app.delete("/:id", async (req,res)=>{
-    const eliminar = await usuario.deleteOne({_id:req.params.id})
-    if(eliminar){
-        res.status(200).json({"mensaje":"eliminado exitoso"})
-    }else{
-        res.status(404).json({"mensaje":"no se realizo"})
+app.put("/productos/:id", async (req, res) => {
+    const productEditar = {
+        referencia: req.body.referencia,
+        nombre: req.body.nombreProducto,
+        descripcion: req.body.descripcion,
+        precio: req.body.precio,
+        stock: req.body.stock,
+        Imagen: req.body.imagen,
+        habilitado: req.body.habilitado
+    }
+    let actualizar = await productos.findOneAndUpdate({ _id: req.params.id }, productEditar)
+    if (actualizar) {
+        res.status(200).json({ "mensaje": "actualizado exitoso" })
+    } else {
+        res.status(404).json({ "mensaje": "no se realizo" })
+
     }
 })
 
-app.listen(process.env.PORT, ()=>{
+app.delete("/productos/:id", async (req, res) => {
+    const eliminar = await productos.deleteOne({ _id: req.params.id })
+    if (eliminar) {
+        res.status(200).json({ "mensaje": "eliminado exitoso" })
+    } else {
+        res.status(404).json({ "mensaje": "no se realizo" })
+    }
+});
+
+//clientes
+
+app.get("/clientes", async (req, res) => {
+    const consulta = await clientes.find({})
+    res.status(200).json(consulta)
+})
+
+app.get("/clientes/:id", async (req, res) => {
+    let consulta = await clientes.findOne({ _id: req.params.id })
+    res.status(200).json(consulta)
+})
+
+app.post("/clientes", async (req, res) => {
+    const nuevoClientes = {
+        referencia: req.body.referencia,
+        nombre: req.body.nombre,
+        descripcion: req.body.descripcion,
+        precio: req.body.precio,
+        stock: req.body.stock,
+        imagen: req.body.imagen,
+        habilitado: req.body.habilitado
+    }
+
+    const insercion = await clientes.create(nuevoClientes)
+    if (insercion) {
+        res.status(200).json({ "mensaje": "registro realizado" })
+    } else {
+        res.status(404).json({ "mensaje": "no se realizo" })
+
+    }
+})
+
+app.put("/:id", async (req, res) => {
+    const productEditar = {
+        referencia: req.body.referencia,
+        nombre: req.body.nombreProducto,
+        descripcion: req.body.descripcion,
+        precio: req.body.precio,
+        stock: req.body.stock,
+        Imagen: req.body.imagen,
+        habilitado: req.body.habilitado
+    }
+    let actualizar = await clientes.findOneAndUpdate({ _id: req.params.id }, productEditar)
+    if (actualizar) {
+        res.status(200).json({ "mensaje": "actualizado exitoso" })
+    } else {
+        res.status(404).json({ "mensaje": "no se realizo" })
+
+    }
+})
+
+app.delete("/:id", async (req, res) => {
+    const eliminar = await clientes.deleteOne({ _id: req.params.id })
+    if (eliminar) {
+        res.status(200).json({ "mensaje": "eliminado exitoso" })
+    } else {
+        res.status(404).json({ "mensaje": "no se realizo" })
+    }
+});
+const emailS = require("./backend/utils/email.services")
+
+app.get('/enviarCorreo', async (req, res) => {
+    console.log('h')
+    await emailS.sendEmail(
+        "edwardstev372@gmail.com",
+         "Mastubino",
+        "TERRIBLE PAJERO",
+    );
+})
+
+app.listen(process.env.PORT, () => {
     console.log(process.env.PORT)
 })
